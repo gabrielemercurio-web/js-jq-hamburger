7b6bb10af89f59c3ed624962b232bde5aa14fdc8
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-29 17:19:00 +0200

    Esercitazione hamburger-menu Completata

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 
-
 // Aggiungere il codice js necessario per visualizzare l'hamburger menu su mobile, a partire da html e css già pronti.
+
+$('.fa-bars').click(function() {
+    $('.hamburger-menu').show(200);
+});
+
+$('.fa-times').click(function() {
+    $('.hamburger-menu').hide(200);
+})
