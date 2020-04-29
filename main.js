e99c38a1f78cf10bf8cb71e48106a07039ef1449
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-29 17:47:03 +0200

    Ottimizzazione Bonus Style.css

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 
 // Aggiungere il codice js necessario per visualizzare l'hamburger menu su mobile, a partire da html e css già pronti.
+//
+// $('.fa-bars').click(function() {
+//     $('.hamburger-menu').show(200);
+// });
+//
+// $('.fa-times').click(function() {
+//     $('.hamburger-menu').hide(200);
+// })
+//
 
-$('.fa-bars').click(function() {
-    $('.hamburger-menu').show(200);
+$('.fa-bars').click(function()
+{
+    $('.hamburger-menu').addClass('hamburger-menu-visible');
 });
 
-$('.fa-times').click(function() {
-    $('.hamburger-menu').hide(200);
+$('.fa-times').click(function()
+{
+    $('.hamburger-menu').removeClass('hamburger-menu-visible');
 })
